b47df7f4782d24bc8f2b64aa85b2f9aaa97c9abe
William Andrew Franck <43352447+willfranck@users.noreply.github.com>
2024-10-22 23:47:06 -0400

    Fix: Added check for window

--- src/hooks/global.ts
+++ src/hooks/global.ts
@@ -1,11 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 const useIsMobileOS = () => {
-  const userAgent = navigator.userAgent || (window as any).opera
+  if (typeof window !== "undefined") {
+    const userAgent = navigator.userAgent || (window as any).opera
 
-  const isIOS = /iPad|iPhone|iPod/.test(userAgent)
-  const isAndroid = /android/i.test(userAgent)
-  
-  return isIOS || isAndroid
+    const isIOS = /iPad|iPhone|iPod/.test(userAgent)
+    const isAndroid = /android/i.test(userAgent)
+    
+    return isIOS || isAndroid
+  }
 }
 
 export {
