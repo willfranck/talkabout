c8151d25be35658647ad38d6541d6e37c5ef4c9d
William Andrew Franck <43352447+willfranck@users.noreply.github.com>
2024-10-28 17:42:33 -0400

    Fix: Added placeholder Profile page

---
+++ src/app/(pages)/profile/page.tsx
@@ -0,0 +1,10 @@
+import { PageLayout} from "@ui/mui-layout"
+
+
+export default function ProfilePage() {
+  return (
+    <PageLayout>
+      <h1 className="text-2xl font-bold mb-16">Profile</h1>
+    </PageLayout>
+  )
+}
