73befaec9fe1e5864b75b5f3fe9d71bd0c888909
William Andrew Franck <43352447+willfranck@users.noreply.github.com>
2025-08-17 17:11:43 -0400

    Build: Removed Electron and related deps

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
